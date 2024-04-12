c2ea627ef3759ef517eafad1efbdb8d6e6debc85
SlavamirMartynkin <129394288+SlavamirMartynkin@users.noreply.github.com>
2024-04-12 17:17:04 +0300

    Update Program.cs

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+using System.Diagnostics;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -24,26 +25,36 @@ namespace ChatClient
                     messageTextOut = Console.ReadLine();
                 }
                 while (string.IsNullOrEmpty(messageTextOut));
-
-                Message message = new Message() { 
-                    DateTime = DateTime.Now, 
-                    NickNameFrom = myNickName, 
-                    NickNameTo = "Server", 
-                    Text = messageTextOut };
-                string json = message.SerializeMessageToJson();
-
-                byte[] data = Encoding.UTF8.GetBytes(json);
-                udpClient.Send(data, data.Length, iPEndPoint);
-                
-                byte[] buffer = udpClient.Receive(ref iPEndPoint);  
-                if (buffer != null)
+                if (messageTextOut == "Exit")
                 {
-                    var messageTextIn = Encoding.UTF8.GetString(buffer);
-                    Console.WriteLine(messageTextIn);
-                    Console.WriteLine();
+                    Console.WriteLine("Чат закрыт.");
+                    Thread.Sleep(1000);
+                    Process.GetCurrentProcess().Kill();
                 }
-                else Console.WriteLine("Сообщение не дошло(((");
+                else
+                {
 
+                    Message message = new Message()
+                    {
+                        DateTime = DateTime.Now,
+                        NickNameFrom = myNickName,
+                        NickNameTo = "Server",
+                        Text = messageTextOut
+                    };
+                    string json = message.SerializeMessageToJson();
+
+                    byte[] data = Encoding.UTF8.GetBytes(json);
+                    udpClient.Send(data, data.Length, iPEndPoint);
+
+                    byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                    if (buffer != null)
+                    {
+                        var messageTextIn = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine(messageTextIn);
+                        Console.WriteLine();
+                    }
+                    else Console.WriteLine("Сообщение не дошло(((");
+                }
             }
         }
     }
